b49566b6746f17ce80efc8bf2d62c24e2cd494c4
lostleaf <lostleaf@me.com>
2020-06-18 20:42:55 +0800

    Add a function to merge all hdfs

--- cli.py
+++ cli.py
@@ -68,7 +68,7 @@ class Main:
             end_date = begin_date
         begin_date, end_date = pd.to_datetime(str(begin_date)), pd.to_datetime(str(end_date))
         dates = os.listdir(os.path.join(DATA_DIR, exchange, symbol_type))
-        dates = sorted(d for d in dates if begin_date <= pd.to_datetime(d) <= end_date)
+        dates = sorted(d for d in dates if begin_date <= pd.to_datetime(d) <= end_date and not d.endswith('.hdf'))
         for date in dates:
             logging.info(f'Converting {date}')
             paths = glob.glob(os.path.join(DATA_DIR, exchange, symbol_type, date, '*.csv'))
@@ -77,5 +77,17 @@ class Main:
                 df['candle_begin_time'] = pd.to_datetime(df['candle_begin_time'])
                 hdf_path = f'{path[:-4]}.hdf'
                 update_data(hdf_path, df.set_index('candle_begin_time'))
+
+    def merge_hdfs(self, exchange, symbol_type):
+        dates = os.listdir(os.path.join(DATA_DIR, exchange, symbol_type))
+        dates = sorted(d for d in dates if not d.endswith('.hdf'))
+        paths = glob.glob(os.path.join(DATA_DIR, exchange, symbol_type, '*', '*.hdf'))
+        names = set(os.path.basename(p).split('.')[0] for p in paths)
+        for name in names:
+            logging.info(f'Merging {name}')
+            paths = glob.glob(os.path.join(DATA_DIR, exchange, symbol_type, '*', f'{name}.hdf'))
+            dfs = [pd.read_hdf(p) for p in paths]
+            df = pd.concat(dfs).sort_index()
+            df.to_hdf(os.path.join(DATA_DIR, exchange, symbol_type, f'{name}.hdf'), 'data')
 
 fire.Fire(Main)
